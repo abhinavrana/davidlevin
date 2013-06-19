77bc5d167ead4bb0aa5fa49544894e7a02db0f05
abhinav rana <abhinav.rana1990@gmail.com>
2013-06-20 00:23:01 +0530

    test 2 commit

--- wp-content/themes/davidlevin/footer.php
+++ wp-content/themes/davidlevin/footer.php
@@ -18,7 +18,37 @@
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
+<script type="text/javascript">
+var counter = 0;
+$('.single-block-img').hover(function(){
+counter++;
+$(this).parent().find('div.animateme').removeClass('animateme').addClass('animateme-current'+counter);
+animateIt(counter);
+});
+function animateIt(counter){
+var txt = $('.animateme-current'+counter).text();
+var timeOut;
+var txtLen = txt.length;
+var char = 0;
+$('.animateme-current'+counter).text('|');
+(function typeIt() {
+var humanize = Math.round(Math.random() * (200 - 30)) + 30;
+timeOut = setTimeout(function() {
+char++;
+var type = txt.substring(0, char);
+$('.animateme-current'+counter).text(type + '|');
+typeIt();
 
+if (char == txtLen) {
+$('.animateme-current'+counter).text($('.animateme-current'+counter).text().slice(0, -1)); // remove the '|'
+clearTimeout(timeOut);
+}
+
+}, 22);
+}());
+
+}
+</script>
 <?php wp_footer(); ?>
 </body>
 </html>
